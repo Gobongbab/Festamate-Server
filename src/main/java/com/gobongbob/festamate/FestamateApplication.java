d7144e281340cbc8a6fc8952301d29c093db026e
jae woo Jung <awfjol2008@gmail.com>
2025-01-31 18:03:48 +0900

    프로젝트 기본 세팅

---
+++ src/main/java/com/gobongbob/festamate/FestamateApplication.java
@@ -0,0 +1,13 @@
+package com.gobongbob.festamate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestamateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestamateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gobongbob/festamate/FestamateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobongbob.festamate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestamateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
