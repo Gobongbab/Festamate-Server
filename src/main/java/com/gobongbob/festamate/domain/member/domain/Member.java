0064b67a49495bc083f39a717c84417a2a94e590
jae woo Jung <awfjol2008@gmail.com>
2025-02-05 21:27:00 +0900

    feat: 회원 엔티티에 학과 정보 추가

--- src/main/java/com/gobongbob/festamate/domain/member/domain/Member.java
+++ src/main/java/com/gobongbob/festamate/domain/member/domain/Member.java
@@ -1,11 +1,14 @@
 package com.gobongbob.festamate.domain.member.domain;
 
+import com.gobongbob.festamate.domain.department.domain.Department;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Generated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
 @Entity
@@ -32,4 +35,8 @@ public class Member {
     private String phoneNumber;
 
     private Gender gender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
 }
