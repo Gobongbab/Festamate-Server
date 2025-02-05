aac1860bc2d002e7b6ca865979d4cacee4b3e600
jae woo Jung <awfjol2008@gmail.com>
2025-02-05 21:10:22 +0900

    feat: 사용자 제재 정보를 관리하는 Ban 엔티티 구현

---
+++ src/main/java/com/gobongbob/festamate/domain/ban/domain/Ban.java
@@ -0,0 +1,25 @@
+package com.gobongbob.festamate.domain.ban.domain;
+
+import com.gobongbob.festamate.domain.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Ban {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String reason;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
