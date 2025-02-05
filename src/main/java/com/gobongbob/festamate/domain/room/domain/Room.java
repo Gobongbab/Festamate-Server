375edc8592f8d7a1054f2fce428be8dba501b06b
jae woo Jung <awfjol2008@gmail.com>
2025-02-05 21:09:25 +0900

    feat: 모임방 정보를 관리하는 Room 엔티티 구현

---
+++ src/main/java/com/gobongbob/festamate/domain/room/domain/Room.java
@@ -0,0 +1,38 @@
+package com.gobongbob.festamate.domain.room.domain;
+
+import com.gobongbob.festamate.domain.member.domain.Gender;
+import com.gobongbob.festamate.domain.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int headCount;
+
+    private Gender preferredGender;
+
+    private String openChatLink;
+
+    private LocalDateTime meetingDateTime;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "host_id")
+    private Member host;
+}
