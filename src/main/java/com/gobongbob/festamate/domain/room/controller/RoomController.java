9e34eb2d31a55de5f767b171414091858709d1ad
jae woo Jung <awfjol2008@gmail.com>
2025-02-22 18:55:38 +0900

    feat: 모임방 생성 API 구현

--- src/main/java/com/gobongbob/festamate/domain/room/controller/RoomController.java
+++ src/main/java/com/gobongbob/festamate/domain/room/controller/RoomController.java
@@ -1,8 +1,12 @@
 package com.gobongbob.festamate.domain.room.controller;
 
+import com.gobongbob.festamate.domain.room.dto.request.RoomCreateRequest;
 import com.gobongbob.festamate.domain.room.service.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/rooms")
 public class RoomController {
     private final RoomService roomService;
+
+    @GetMapping("")
+    public ResponseEntity<Void> createRoom(@RequestBody RoomCreateRequest request) {
+        roomService.createRoom(request);
+
+        return ResponseEntity.ok().build();
+    }
 }
