8d69989de3fdcea1c9ffc56c91ac62292e4f0ead
jae woo Jung <awfjol2008@gmail.com>
2025-02-24 21:07:50 +0900

    feat: 모임방 수정 APi 구현

--- src/main/java/com/gobongbob/festamate/domain/room/controller/RoomController.java
+++ src/main/java/com/gobongbob/festamate/domain/room/controller/RoomController.java
@@ -1,6 +1,7 @@
 package com.gobongbob.festamate.domain.room.controller;
 
 import com.gobongbob.festamate.domain.room.dto.request.RoomCreateRequest;
+import com.gobongbob.festamate.domain.room.dto.request.RoomUpdateRequest;
 import com.gobongbob.festamate.domain.room.dto.response.RoomResponse;
 import com.gobongbob.festamate.domain.room.service.RoomService;
 import java.util.List;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,5 +37,15 @@ public class RoomController {
     @GetMapping("/{roomId}")
     public ResponseEntity<RoomResponse> findRoomById(@PathVariable Long roomId) {
         return ResponseEntity.ok(roomService.findRoomById(roomId));
+    }
+
+    @PatchMapping("/{roomId}")
+    public ResponseEntity<Void> updateRoomById(
+            @PathVariable Long roomId,
+            @RequestBody RoomUpdateRequest request
+    ) {
+        roomService.updateRoomById(roomId, request);
+
+        return ResponseEntity.ok().build();
     }
 }
