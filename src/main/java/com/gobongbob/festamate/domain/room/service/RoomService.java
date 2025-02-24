8620cf272e4e767ef32ccfec7587b03d810e194b
jae woo Jung <awfjol2008@gmail.com>
2025-02-24 21:06:01 +0900

    feat: 모임방 통합 수정 메서드 구현

--- src/main/java/com/gobongbob/festamate/domain/room/service/RoomService.java
+++ src/main/java/com/gobongbob/festamate/domain/room/service/RoomService.java
@@ -1,7 +1,9 @@
 package com.gobongbob.festamate.domain.room.service;
 
+import com.gobongbob.festamate.domain.member.domain.Gender;
 import com.gobongbob.festamate.domain.room.domain.Room;
 import com.gobongbob.festamate.domain.room.dto.request.RoomCreateRequest;
+import com.gobongbob.festamate.domain.room.dto.request.RoomUpdateRequest;
 import com.gobongbob.festamate.domain.room.dto.response.RoomResponse;
 import com.gobongbob.festamate.domain.room.repository.RoomRepository;
 import java.util.List;
@@ -34,5 +36,27 @@ public class RoomService {
         return roomRepository.findById(roomId)
                 .map(RoomResponse::toDto)
                 .orElseThrow(() -> new IllegalArgumentException("해당 방이 존재하지 않습니다."));
+    }
+
+    public void updateRoomById(Long roomId, RoomUpdateRequest request) {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 방이 존재하지 않습니다."));
+
+        /*
+        모임방 정보를 수정할 수 있는 조건인지 확인하는 로직이 추후 추가되어야 합니다.
+        1. 요청한 사용자가 방장인지 확인한다.
+        2. 방에 방장을 제외한 다른 사용자가 입장하지 않은 것을 확인한다.
+         */
+
+        room.updateRoom(
+                request.headCount(),
+                Gender.findByName(request.preferredGender()),
+                request.openChatLink(),
+                request.meetingDateTime(),
+                request.title(),
+                request.content()
+        );
+
+        roomRepository.save(room);
     }
 }
