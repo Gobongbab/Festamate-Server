1b31e10ab8dc7a8f906da6d9303148b9dc18d739
jae woo Jung <awfjol2008@gmail.com>
2025-02-24 20:45:21 +0900

    feat: 모임방 전체 조회 API 구현

--- src/main/java/com/gobongbob/festamate/domain/room/controller/RoomController.java
+++ src/main/java/com/gobongbob/festamate/domain/room/controller/RoomController.java
@@ -1,11 +1,14 @@
 package com.gobongbob.festamate.domain.room.controller;
 
 import com.gobongbob.festamate.domain.room.dto.request.RoomCreateRequest;
+import com.gobongbob.festamate.domain.room.dto.response.RoomResponse;
 import com.gobongbob.festamate.domain.room.service.RoomService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,12 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/api/rooms")
 public class RoomController {
+
     private final RoomService roomService;
 
-    @GetMapping("")
+    @PostMapping("")
     public ResponseEntity<Void> createRoom(@RequestBody RoomCreateRequest request) {
         roomService.createRoom(request);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<RoomResponse>> findAllRooms() {
+        return ResponseEntity.ok(roomService.findRooms());
     }
 }
