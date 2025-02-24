168c1c592dce628c44452f87046a7bd6612e8879
jae woo Jung <awfjol2008@gmail.com>
2025-02-24 20:42:28 +0900

    feat: 모임방 전체 조회 쿼리 메서드 구현

--- src/main/java/com/gobongbob/festamate/domain/room/repository/RoomRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/room/repository/RoomRepository.java
@@ -1,12 +1,15 @@
 package com.gobongbob.festamate.domain.room.repository;
 
 import com.gobongbob.festamate.domain.room.domain.Room;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
-import java.util.Optional;
-
 public interface RoomRepository extends Repository<Long, Room> {
+
     Room save(Room room);
+
+    List<Room> findAll();
 
     Optional<Room> findById(Long id);
 
