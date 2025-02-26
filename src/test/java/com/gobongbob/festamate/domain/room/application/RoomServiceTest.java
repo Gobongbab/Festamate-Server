cd3e481d0bb9febf9bf3cb09e16e47e64cab00d7
jae woo Jung <awfjol2008@gmail.com>
2025-02-26 21:46:26 +0900

    test: 모임방 생성 기능 관련 테스트 케이스 작성

--- src/test/java/com/gobongbob/festamate/domain/room/application/RoomServiceTest.java
+++ src/test/java/com/gobongbob/festamate/domain/room/application/RoomServiceTest.java
@@ -1,9 +1,18 @@
 package com.gobongbob.festamate.domain.room.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.gobongbob.festamate.domain.member.domain.Gender;
+import com.gobongbob.festamate.domain.room.domain.Room;
+import com.gobongbob.festamate.domain.room.dto.request.RoomCreateRequest;
+import com.gobongbob.festamate.domain.room.fixture.RoomFixture;
 import com.gobongbob.festamate.domain.room.persistence.RoomRepository;
 import com.gobongbob.festamate.serviceSliceTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 @DisplayName("모임방 관련 기능 테스트")
 class RoomServiceTest extends serviceSliceTest {
@@ -12,4 +21,34 @@ class RoomServiceTest extends serviceSliceTest {
     RoomService roomService;
     @Autowired
     RoomRepository roomRepository;
+
+    @Nested
+    @DisplayName("모임방을 생성할 시")
+    class createRoom {
+
+        @Test
+        @Transactional
+        @DisplayName("모임방을 생성에 성공한다.")
+        void successCreateRoom() {
+            // given
+            Room room = RoomFixture.createRoom(4, Gender.MALE);
+            RoomCreateRequest request = new RoomCreateRequest(
+                    room.getHeadCount(),
+                    room.getPreferredGender().getName(),
+                    room.getOpenChatLink(),
+                    room.getMeetingDateTime(),
+                    room.getTitle(),
+                    room.getContent()
+            );
+
+            // when
+            roomService.createRoom(request);
+            Room findRoom = roomRepository.findById(room.getId())
+                    .orElseThrow(() -> new IllegalArgumentException("모임방이 존재하지 않습니다."));
+
+            // then
+            assertThat(findRoom.getHeadCount()).isEqualTo(room.getHeadCount());
+            assertThat(findRoom.getPreferredGender()).isEqualTo(room.getPreferredGender());
+        }
+    }
 }
