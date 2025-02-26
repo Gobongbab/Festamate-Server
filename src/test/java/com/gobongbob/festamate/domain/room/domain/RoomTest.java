844e6e8172c816494be2a7131c09f237e33e266e
jae woo Jung <awfjol2008@gmail.com>
2025-02-26 21:44:46 +0900

    test: Room 엔티티 테스트 클래스 기본 구현

---
+++ src/test/java/com/gobongbob/festamate/domain/room/domain/RoomTest.java
@@ -0,0 +1,10 @@
+package com.gobongbob.festamate.domain.room.domain;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class RoomTest {
+
+}
