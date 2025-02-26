8a30d08518da6f3cfb0e1c607fcd2d142b6a10c6
jae woo Jung <awfjol2008@gmail.com>
2025-02-26 21:41:43 +0900

    test: Service 레이어 테스트를 위한 슬라이스 테스트 클래스 구현

---
+++ src/test/java/com/gobongbob/festamate/serviceSliceTest.java
@@ -0,0 +1,10 @@
+package com.gobongbob.festamate;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
+public abstract class serviceSliceTest {
+
+}
