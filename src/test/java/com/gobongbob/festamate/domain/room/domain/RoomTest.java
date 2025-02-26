37ec83bbc4d693604fbf7edbd21d022dbf1e68a6
jae woo Jung <awfjol2008@gmail.com>
2025-02-26 21:45:31 +0900

    test: Room 엔티티 자체 수정 기능 관련 테스트케이스 작성

--- src/test/java/com/gobongbob/festamate/domain/room/domain/RoomTest.java
+++ src/test/java/com/gobongbob/festamate/domain/room/domain/RoomTest.java
@@ -1,10 +1,46 @@
 package com.gobongbob.festamate.domain.room.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.gobongbob.festamate.domain.member.domain.Gender;
+import com.gobongbob.festamate.domain.room.fixture.RoomFixture;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class RoomTest {
 
+    @Test
+    @DisplayName("모임방_객체를_수정한다")
+    void updateRoom() {
+        // given
+        Room room = RoomFixture.createRoom(4, Gender.MALE);
+
+        // when
+        room.updateRoom(
+                6,
+                Gender.FEMALE,
+                room.getOpenChatLink(),
+                room.getMeetingDateTime(),
+                room.getTitle(),
+                room.getContent()
+        );
+
+        // then
+        assertThat(room.getHeadCount()).isEqualTo(6);
+        assertThat(room.getPreferredGender()).isEqualTo(Gender.FEMALE);
+    }
+
+    /*
+    다음 테스트 케이스들이 추가로 작성되어야 합니다.
+
+    - 방장이 아닌 사용자가 모임방 정보를 수정하려는 경우, 예외를 발생시킨다.
+    - 방장 외의 인원이 입장한 방의 정보를 수정하려는 경우, 예외를 발생시킨다.
+
+    이 외에도 전체 인원을 짝수로 통일할 것인지, 남성과 여성을 동일한 비율로 맞출 것인지 등에 대한 논의 후
+    테스트 케이스가 작성되어야 합니다.
+     */
 }
