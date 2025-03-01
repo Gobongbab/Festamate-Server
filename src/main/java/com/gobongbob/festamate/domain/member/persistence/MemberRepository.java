187185932734bd60b55adfb4b9e8958fb3f51b93
jae woo Jung <awfjol2008@gmail.com>
2025-03-01 21:10:01 +0900

    feat: 회원 관련 CRUD 메서드 구현

--- src/main/java/com/gobongbob/festamate/domain/member/persistence/MemberRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/member/persistence/MemberRepository.java
@@ -1,8 +1,18 @@
 package com.gobongbob.festamate.domain.member.persistence;
 
 import com.gobongbob.festamate.domain.member.domain.Member;
+import com.gobongbob.festamate.domain.room.domain.Room;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface MemberRepository extends Repository<Member, Long> {
 
+    Member save(Room room);
+
+    List<Member> findAll();
+
+    Optional<Member> findById(Long id);
+
+    void delete(Member room);
 }
