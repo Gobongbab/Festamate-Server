7f960dca9ff818a8f900886883275fb24785a8b9
jae woo Jung <awfjol2008@gmail.com>
2025-03-01 21:32:12 +0900

    feat: 회원 생성 요청 DTO 구현

--- src/main/java/com/gobongbob/festamate/domain/member/persistence/MemberRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/member/persistence/MemberRepository.java
@@ -1,14 +1,13 @@
 package com.gobongbob.festamate.domain.member.persistence;
 
 import com.gobongbob.festamate.domain.member.domain.Member;
-import com.gobongbob.festamate.domain.room.domain.Room;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface MemberRepository extends Repository<Member, Long> {
 
-    Member save(Room room);
+    Member save(Member room);
 
     List<Member> findAll();
 
