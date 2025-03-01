a8bcc9f6b1d0d320bd455210d88851424e3df544
jae woo Jung <awfjol2008@gmail.com>
2025-03-01 21:50:56 +0900

    feat: 회원가입 API 구현

--- src/main/java/com/gobongbob/festamate/domain/member/presentation/MemberController.java
+++ src/main/java/com/gobongbob/festamate/domain/member/presentation/MemberController.java
@@ -1,7 +1,12 @@
 package com.gobongbob.festamate.domain.member.presentation;
 
 import com.gobongbob.festamate.domain.member.application.MemberService;
+import com.gobongbob.festamate.domain.member.domain.Member;
+import com.gobongbob.festamate.domain.member.dto.request.MemberCreateRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signUp(@RequestBody MemberCreateRequest request) {
+        Member member = memberService.createMember(request);
+
+        return ResponseEntity.ok().build();
+    }
 }
