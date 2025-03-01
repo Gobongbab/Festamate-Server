de16d0dae43d40e910a4006d2261c3885f32c158
jae woo Jung <awfjol2008@gmail.com>
2025-03-01 21:35:39 +0900

    feat: 회원 생성 메서드 구현

--- src/main/java/com/gobongbob/festamate/domain/member/application/MemberService.java
+++ src/main/java/com/gobongbob/festamate/domain/member/application/MemberService.java
@@ -1,5 +1,8 @@
 package com.gobongbob.festamate.domain.member.application;
 
+import com.gobongbob.festamate.domain.member.domain.Member;
+import com.gobongbob.festamate.domain.member.dto.request.MemberCreateRequest;
+import com.gobongbob.festamate.domain.member.persistence.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member createMember(MemberCreateRequest request) {
+        Member member = request.toEntity();
+
+        return memberRepository.save(member);
+    }
 }
