ae77ca32ff75fce44dd758e22bf12285886ebd9a
jae woo Jung <awfjol2008@gmail.com>
2025-03-02 23:22:52 +0900

    feat: 기존 Department 클래스를 Major Enum으로 변환 및 관련 변경사항 수정

--- src/main/java/com/gobongbob/festamate/domain/member/domain/Member.java
+++ src/main/java/com/gobongbob/festamate/domain/member/domain/Member.java
@@ -1,14 +1,13 @@
 package com.gobongbob.festamate.domain.member.domain;
 
-import com.gobongbob.festamate.domain.department.domain.Department;
+import com.gobongbob.festamate.domain.major.domain.Major;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -44,7 +43,6 @@ public class Member {
 
     private Gender gender;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "department_id")
-    private Department department;
+    @Enumerated(EnumType.STRING)
+    private Major major;
 }
