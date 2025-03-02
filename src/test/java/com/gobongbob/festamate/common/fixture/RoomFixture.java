9599986a03ebebe2d71bb8d617064cddd59a519b
jae woo Jung <awfjol2008@gmail.com>
2025-03-03 01:29:48 +0900

    test: 테스트용 모임방 객체 생성 시, 호스트 정보를 같이 넣도록 추가

--- src/test/java/com/gobongbob/festamate/common/fixture/RoomFixture.java
+++ src/test/java/com/gobongbob/festamate/common/fixture/RoomFixture.java
@@ -1,6 +1,7 @@
 package com.gobongbob.festamate.domain.room.fixture;
 
 import com.gobongbob.festamate.domain.member.domain.Gender;
+import com.gobongbob.festamate.domain.member.domain.Member;
 import com.gobongbob.festamate.domain.room.domain.Room;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -8,7 +9,7 @@ import java.util.List;
 
 public class RoomFixture {
 
-    public static Room createRoom(int headCount, Gender gender) {
+    public static Room createRoom(int headCount, Gender gender, Member member) {
         return Room.builder()
                 .headCount(headCount)
                 .preferredGender(gender)
@@ -16,13 +17,14 @@ public class RoomFixture {
                 .meetingDateTime(LocalDateTime.now())
                 .title("test title")
                 .content("test content")
+                .host(member)
                 .build();
     }
 
-    public static List<Room> createRooms() {
+    public static List<Room> createRooms(Member member) {
         List<Room> rooms = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            rooms.add(createRoom(2 * (i + 1), Gender.MALE));
+            rooms.add(createRoom(2 * (i + 1), Gender.MALE, member));
         }
 
         return rooms;
