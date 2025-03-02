0cc471ae5903ea4e84e7443554178b28f073a82d
jae woo Jung <awfjol2008@gmail.com>
2025-03-02 23:58:40 +0900

    test: MemberFixture 구현

---
+++ src/test/java/com/gobongbob/festamate/domain/member/fixture/MemberFixture.java
@@ -0,0 +1,28 @@
+package com.gobongbob.festamate.domain.member.fixture;
+
+import com.gobongbob.festamate.domain.major.domain.Major;
+import com.gobongbob.festamate.domain.member.domain.Gender;
+import com.gobongbob.festamate.domain.member.domain.Member;
+
+public class MemberFixture {
+
+    public static Member createMember(
+            String nickname,
+            String studentId,
+            String loginId,
+            String phoneNumber,
+            Major major
+    ) {
+        return Member.builder()
+                .name("testName1")
+                .nickname(nickname)
+                .studentId(studentId)
+                .loginId(loginId)
+                .loginPassword("testPassword1")
+                .phoneNumber(phoneNumber)
+                .gender(Gender.MALE)
+                .major(major)
+                .build();
+    }
+
+}
