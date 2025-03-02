15846a0cd5472ec5e74e1945a8de9c54a4e7c5dc
jae woo Jung <awfjol2008@gmail.com>
2025-03-02 23:22:52 +0900

    feat: 기존 Department 클래스를 Major Enum으로 변환 및 관련 변경사항 수정

---
+++ src/main/java/com/gobongbob/festamate/domain/major/domain/Major.java
@@ -0,0 +1,23 @@
+package com.gobongbob.festamate.domain.major.domain;
+
+import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Major {
+    COMPUTER_SCIENCE("소프트웨어융합대학", "AI컴퓨터공학부"),
+    BUSINESS_ADMINISTRATION("소프트웨어융합대학", "경영학부"),
+    INDUSTRIAL_MANAGEMENT_INFORMATION("소프트웨어융합대학", "컴퓨터공학과");
+
+    private final String college;
+    private final String department;
+
+    public static Major findByName(String department) {
+        return Arrays.stream(Major.values())
+                .filter(major -> major.getDepartment().equals(department))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 학과가 존재하지 않습니다."));
+    }
+}
