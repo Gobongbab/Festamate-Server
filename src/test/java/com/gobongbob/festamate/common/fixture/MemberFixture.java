cea7448a818ed3b74bd5981050fabde387f0e169
jae woo Jung <awfjol2008@gmail.com>
2025-03-03 21:51:17 +0900

    test: 테스트용 회원 객체 생성 메서드 추가 구현

--- src/test/java/com/gobongbob/festamate/common/fixture/MemberFixture.java
+++ src/test/java/com/gobongbob/festamate/common/fixture/MemberFixture.java
@@ -3,6 +3,7 @@ package com.gobongbob.festamate.common.fixture;
 import com.gobongbob.festamate.domain.major.domain.Major;
 import com.gobongbob.festamate.domain.member.domain.Gender;
 import com.gobongbob.festamate.domain.member.domain.Member;
+import java.util.List;
 
 public class MemberFixture {
 
@@ -25,13 +26,41 @@ public class MemberFixture {
                 .build();
     }
 
+    public static List<Member> createMembers() {
+        return List.of(
+                MEMBER1(),
+                MEMBER2(),
+                MEMBER3()
+        );
+    }
+
     public static Member MEMBER1() {
         return createMember(
                 "testNickname1",
                 "202500001",
                 "testLoginId1",
-                "01012345678",
+                "01011111111",
                 Major.COMPUTER_SCIENCE
+        );
+    }
+
+    public static Member MEMBER2() {
+        return createMember(
+                "testNickname2",
+                "202500002",
+                "testLoginId2",
+                "01022222222",
+                Major.BUSINESS_ADMINISTRATION
+        );
+    }
+
+    public static Member MEMBER3() {
+        return createMember(
+                "testNickname3",
+                "202500003",
+                "testLoginId3",
+                "01033333333",
+                Major.INDUSTRIAL_MANAGEMENT_INFORMATION
         );
     }
 }
