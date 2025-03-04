1919d85f5fb74c96e8ced657f67eca2138197a08
jae woo Jung <awfjol2008@gmail.com>
2025-03-04 20:13:04 +0900

    test: 회원 단건 조회 관련 테스트 케이스 작성

--- src/test/java/com/gobongbob/festamate/domain/member/application/MemberServiceTest.java
+++ src/test/java/com/gobongbob/festamate/domain/member/application/MemberServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import com.gobongbob.festamate.common.fixture.MemberFixture;
 import com.gobongbob.festamate.domain.member.domain.Member;
 import com.gobongbob.festamate.domain.member.dto.request.MemberCreateRequest;
+import com.gobongbob.festamate.domain.member.dto.response.MemberResponse;
 import com.gobongbob.festamate.domain.member.persistence.MemberRepository;
 import com.gobongbob.festamate.serviceSliceTest;
 import org.junit.jupiter.api.DisplayName;
@@ -43,7 +44,36 @@ class MemberServiceTest extends serviceSliceTest {
                     () -> assertThat(createdMember.getNickname()).isEqualTo(member.getNickname()),
                     () -> assertThat(createdMember.getStudentId()).isEqualTo(member.getStudentId()),
                     () -> assertThat(createdMember.getLoginId()).isEqualTo(member.getLoginId()),
-                    () -> assertThat(createdMember.getPhoneNumber()).isEqualTo(member.getPhoneNumber())
+                    () -> assertThat(createdMember.getPhoneNumber()).isEqualTo(
+                            member.getPhoneNumber())
+            );
+        }
+    }
+
+    @Nested
+    @DisplayName("회원을 조회할 시")
+    class findMember {
+
+        @Test
+        @DisplayName("특정 회원 단건 조회에 성공한다.")
+        void successFindMemberById() {
+            // given
+            Member member = testFixtureBuilder.buildMember(MemberFixture.MEMBER1());
+
+            // when
+            MemberResponse response = memberService.findMemberById(member.getId());
+
+            // then
+            assertAll(
+                    () -> assertThat(response.id()).isNotNull(),
+                    () -> assertThat(response.name()).isEqualTo(member.getName()),
+                    () -> assertThat(response.nickname()).isEqualTo(member.getNickname()),
+                    () -> assertThat(response.studentId()).isEqualTo(member.getStudentId()),
+                    () -> assertThat(response.loginId()).isEqualTo(member.getLoginId()),
+                    () -> assertThat(response.loginPassword()).isEqualTo(member.getLoginPassword()),
+                    () -> assertThat(response.phoneNumber()).isEqualTo(member.getPhoneNumber()),
+                    () -> assertThat(response.gender()).isEqualTo(member.getGender().getName()),
+                    () -> assertThat(response.major()).isEqualTo(member.getMajor().getDepartment())
             );
         }
     }
