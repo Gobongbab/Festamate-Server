d65ab10ec8c8325b016f1929dbac074c41d6eda8
jae woo Jung <awfjol2008@gmail.com>
2025-03-04 18:12:15 +0900

    feat: 회원 전체 조회 API 구현

--- src/main/java/com/gobongbob/festamate/domain/member/presentation/MemberController.java
+++ src/main/java/com/gobongbob/festamate/domain/member/presentation/MemberController.java
@@ -3,24 +3,30 @@ package com.gobongbob.festamate.domain.member.presentation;
 import com.gobongbob.festamate.domain.member.application.MemberService;
 import com.gobongbob.festamate.domain.member.domain.Member;
 import com.gobongbob.festamate.domain.member.dto.request.MemberCreateRequest;
+import com.gobongbob.festamate.domain.member.dto.response.MemberResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/signup")
+    @PostMapping("/auth/signup")
     public ResponseEntity<Void> signUp(@RequestBody MemberCreateRequest request) {
         Member member = memberService.createMember(request);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<MemberResponse>> findAllMembers() {
+        return ResponseEntity.ok(memberService.findAllMembers());
     }
 }
