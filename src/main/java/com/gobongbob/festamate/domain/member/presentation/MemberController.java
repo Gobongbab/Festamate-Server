5e052111e471f89e9f11043b8e66ca052048ec61
jae woo Jung <awfjol2008@gmail.com>
2025-03-04 18:20:37 +0900

    feat: 회원 프로필 조회 API 구현

--- src/main/java/com/gobongbob/festamate/domain/member/presentation/MemberController.java
+++ src/main/java/com/gobongbob/festamate/domain/member/presentation/MemberController.java
@@ -3,11 +3,15 @@ package com.gobongbob.festamate.domain.member.presentation;
 import com.gobongbob.festamate.domain.member.application.MemberService;
 import com.gobongbob.festamate.domain.member.domain.Member;
 import com.gobongbob.festamate.domain.member.dto.request.MemberCreateRequest;
+import com.gobongbob.festamate.domain.member.dto.response.MemberProfileResponse;
 import com.gobongbob.festamate.domain.member.dto.response.MemberResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,5 +38,15 @@ public class MemberController {
     @GetMapping("/members/{memberId}")
     public ResponseEntity<MemberResponse> findMemberById(@PathVariable Long memberId) {
         return ResponseEntity.ok(memberService.findMemberById(memberId));
+    }
+
+    @GetMapping("/members/profile")
+    public ResponseEntity<MemberProfileResponse> getProfile(Long memberId) { // 추후 Spring Security를 활용하여 사용자 정보를 가져오도록 변경 필요
+        return ResponseEntity.ok(memberService.findProfile(memberId));
+    }
+
+    @PatchMapping("/members/profile")
+    public ResponseEntity<Void> updateProfile() {
+
     }
 }
