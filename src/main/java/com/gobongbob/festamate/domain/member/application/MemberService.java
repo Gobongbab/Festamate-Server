3eac7423ab3f67d8d32fa6b541da205dffc4513f
jae woo Jung <awfjol2008@gmail.com>
2025-03-04 17:15:39 +0900

    feat: 회원 전체 조회 메서드 구현

--- src/main/java/com/gobongbob/festamate/domain/member/application/MemberService.java
+++ src/main/java/com/gobongbob/festamate/domain/member/application/MemberService.java
@@ -2,7 +2,9 @@ package com.gobongbob.festamate.domain.member.application;
 
 import com.gobongbob.festamate.domain.member.domain.Member;
 import com.gobongbob.festamate.domain.member.dto.request.MemberCreateRequest;
+import com.gobongbob.festamate.domain.member.dto.response.MemberResponse;
 import com.gobongbob.festamate.domain.member.persistence.MemberRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,5 +21,12 @@ public class MemberService {
         Member member = request.toEntity();
 
         return memberRepository.save(member);
+    }
+
+    public List<MemberResponse> findAllMembers() {
+        return memberRepository.findAll()
+                .stream()
+                .map(MemberResponse::fromEntity)
+                .toList();
     }
 }
