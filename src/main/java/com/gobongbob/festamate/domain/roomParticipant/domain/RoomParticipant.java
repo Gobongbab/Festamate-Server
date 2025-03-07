0018064bf3c22c9c2ea1a978f302f599b1b618ed
jae woo Jung <awfjol2008@gmail.com>
2025-03-08 01:43:30 +0900

    feat: RoomParticipant 엔티티에 호스트 여부 관련 필드 추가

--- src/main/java/com/gobongbob/festamate/domain/roomParticipant/domain/RoomParticipant.java
+++ src/main/java/com/gobongbob/festamate/domain/roomParticipant/domain/RoomParticipant.java
@@ -14,6 +14,7 @@ import lombok.Getter;
 @Entity
 @Getter
 public class RoomParticipant {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,4 +26,6 @@ public class RoomParticipant {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    private boolean isHost;
 }
