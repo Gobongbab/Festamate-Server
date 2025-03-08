a4bc64f38314e59b49d80315be4e0d06c8f0a4f4
Muokok <sgo5178@kyonggi.ac.kr>
2025-03-08 23:46:09 +0900

    refactor: report 엔티티 수정

--- src/main/java/com/gobongbob/festamate/domain/report/domain/Report.java
+++ src/main/java/com/gobongbob/festamate/domain/report/domain/Report.java
@@ -1,29 +1,39 @@
 package com.gobongbob.festamate.domain.report.domain;
 
 import com.gobongbob.festamate.domain.member.domain.Member;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Getter;
+import com.gobongbob.festamate.domain.room.domain.Room;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Report {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String reason;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "reporter_id")
     private Member reporter;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reported_member_id")
-    private Member reportedMember;
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    private String reason;
+
+    @Column(nullable = false)
+    private LocalDateTime reportDate;
+
+    @Column
+    private Boolean processed = false;
+
+    public void markAsProcessed() {
+        this.processed = true;
+    }
 }
