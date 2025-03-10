d74694b257c1152b455ec8291d1909fffc2d50c6
jae woo Jung <awfjol2008@gmail.com>
2025-03-10 16:24:17 +0900

    fix: 모임방 수정 관련 테스트코드 수정

--- src/main/java/com/gobongbob/festamate/domain/room/application/RoomService.java
+++ src/main/java/com/gobongbob/festamate/domain/room/application/RoomService.java
@@ -118,7 +118,7 @@ public class RoomService {
 
     private void validateAlone(Room room) {
         int participantsCount = roomParticipantRepository.countByRoom_Id(room.getId());
-        if (participantsCount != 1) {
+        if (participantsCount > 1) {
             throw new IllegalArgumentException("방에 방장을 제외한 다른 사용자가 입장한 상태에서는 수정할 수 없습니다.");
         }
     }
