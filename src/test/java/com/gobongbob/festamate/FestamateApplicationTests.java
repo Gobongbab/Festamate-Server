c4619259ffed5ae148ad316612638b6cdc49ffae
ahyeonkong <kimah0403@gmail.com>
2025-03-17 21:15:14 +0900

    test: 테스트 profile 활성화

--- src/test/java/com/gobongbob/festamate/FestamateApplicationTests.java
+++ src/test/java/com/gobongbob/festamate/FestamateApplicationTests.java
@@ -2,8 +2,10 @@ package com.gobongbob.festamate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // test 프로파일 활성화
 class FestamateApplicationTests {
 
     @Test
