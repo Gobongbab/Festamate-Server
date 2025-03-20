9f2e5efbb084680b72eade82a2c3eb7c02a7c19a
ahyeonkong <kimah0403@gmail.com>
2025-03-20 20:38:37 +0900

    test: 테스트 환경에서 .env 참조하도록 설정

--- src/test/java/com/gobongbob/festamate/FestamateApplicationTests.java
+++ src/test/java/com/gobongbob/festamate/FestamateApplicationTests.java
@@ -3,9 +3,11 @@ package com.gobongbob.festamate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = FestamateApplication.class)
 @ActiveProfiles("test") // test 프로파일 활성화
+@TestPropertySource(locations = "file:./.env")
 class FestamateApplicationTests {
 
     @Test
