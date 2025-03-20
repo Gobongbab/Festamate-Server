65a7123191644e440a14f6bcde9f63caa4b3e501
ahyeonkong <kimah0403@gmail.com>
2025-03-20 19:10:53 +0900

    test: 테스트 환경 설정을 위한 application-test.yml 추가 및 프로파일 설정 변경

--- src/test/java/com/gobongbob/festamate/FestamateApplicationTests.java
+++ src/test/java/com/gobongbob/festamate/FestamateApplicationTests.java
@@ -1,17 +1,14 @@
 package com.gobongbob.festamate;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = FestamateApplication.class)
 @ActiveProfiles("test") // test 프로파일 활성화
 class FestamateApplicationTests {
 
     @Test
-    @Disabled
-        // 테스트 코드 비활성화
     void contextLoads() {
     }
 
--- src/test/java/com/gobongbob/festamate/serviceSliceTest.java
+++ src/test/java/com/gobongbob/festamate/serviceSliceTest.java
@@ -3,9 +3,11 @@ package com.gobongbob.festamate;
 import com.gobongbob.festamate.common.builder.TestFixtureBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(classes = FestamateApplication.class) // Spring Context 로드
+@ActiveProfiles("test") // 테스트 환경 설정 적용
 @Transactional
 public abstract class serviceSliceTest {
 
