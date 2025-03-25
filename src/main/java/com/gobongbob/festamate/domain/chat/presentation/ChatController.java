afe9574e5c31b1f5eaf3e8fbff0c239c9fa314e7
jae woo Jung <awfjol2008@gmail.com>
2025-03-25 17:07:14 +0900

    feat: 채팅 메시지 브로커 관련 prefix 변경

--- src/main/java/com/gobongbob/festamate/domain/chat/presentation/ChatController.java
+++ src/main/java/com/gobongbob/festamate/domain/chat/presentation/ChatController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +17,7 @@ public class ChatController {
 
     private final ChatService chatService;
 
-    @MessageMapping("/chat/room/{roomId}") // 이거 쓸라면 /publish/chat/{roomId} 엔드포인트 필요
-    @SendTo("/subscribe/room/{roomId}")   //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
+    @MessageMapping("/room/{roomId}") // Spring App 을 거쳐서 메시지 전송. 앞에 "app" prefix 를 붙여야 함
     public ResponseEntity<ChatResponse> chat(
             @DestinationVariable Long roomId,
             @AuthenticationPrincipal Member member,
--- src/main/java/com/gobongbob/festamate/global/config/WebSocketConfig.java
+++ src/main/java/com/gobongbob/festamate/global/config/WebSocketConfig.java
@@ -22,13 +22,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/subscribe");
-        config.setApplicationDestinationPrefixes("/publish");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-connect")
+        registry.addEndpoint("/ws")
                 .setAllowedOrigins("*");
     }
 }
