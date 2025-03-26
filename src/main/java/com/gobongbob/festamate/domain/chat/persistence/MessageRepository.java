6f979a18678502f396d8420eeb56431ca523c1dd
jae woo Jung <awfjol2008@gmail.com>
2025-03-26 16:34:11 +0900

    feat: 모임방을 통한 메시지 조회 쿼리 메서드에 페이징 적용

--- src/main/java/com/gobongbob/festamate/domain/chat/persistence/MessageRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/chat/persistence/MessageRepository.java
@@ -1,12 +1,13 @@
 package com.gobongbob.festamate.domain.chat.persistence;
 
 import com.gobongbob.festamate.domain.chat.domain.Message;
-import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     @Query("select m from Message m where m.room.id = ?1")
-    List<Message> findByRoomId(Long id);
+    Slice<Message> findByRoomId(Long id, Pageable pageable);
 }
