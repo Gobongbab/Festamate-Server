5509482019d28a6ba1016967af77d08634bde1c7
jae woo Jung <awfjol2008@gmail.com>
2025-04-04 17:12:06 +0900

    feat: Room 및 RoomImage 엔티티에 연관관계 편의 메서드 정의

--- src/main/java/com/gobongbob/festamate/domain/room/domain/Room.java
+++ src/main/java/com/gobongbob/festamate/domain/room/domain/Room.java
@@ -1,7 +1,9 @@
 package com.gobongbob.festamate.domain.room.domain;
 
+import com.gobongbob.festamate.domain.image.domain.RoomImage;
 import com.gobongbob.festamate.domain.member.domain.Gender;
 import com.gobongbob.festamate.domain.member.domain.Member;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +11,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -42,6 +47,15 @@ public class Room {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "host_id")
     private Member host;
+
+    @OneToMany(mappedBy = "room", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<RoomImage> images = new ArrayList<>();
+
+    // 연관관계 편의 메서드
+    public void assignImages(List<RoomImage> roomImages) {
+        this.images = roomImages;
+        roomImages.forEach(roomImage -> roomImage.setRoom(this));
+    }
 
     public void updateRoom(
             int headCount,
