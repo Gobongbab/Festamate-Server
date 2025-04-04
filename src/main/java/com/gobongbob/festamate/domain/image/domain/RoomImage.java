3d8a808e6ee4f7f7250c6cd6ed9534d08dd0cc1f
jae woo Jung <awfjol2008@gmail.com>
2025-04-04 17:12:06 +0900

    feat: Room 및 RoomImage 엔티티에 연관관계 편의 메서드 정의

--- src/main/java/com/gobongbob/festamate/domain/image/domain/RoomImage.java
+++ src/main/java/com/gobongbob/festamate/domain/image/domain/RoomImage.java
@@ -1,7 +1,6 @@
 package com.gobongbob.festamate.domain.image.domain;
 
 import com.gobongbob.festamate.domain.room.domain.Room;
-import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,11 +9,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class RoomImage {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,4 +31,15 @@ public class RoomImage {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id")
     private Room room;
+
+    // 연관관계 편의 메서드
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public static RoomImage fromEntity(Image image) {
+        return RoomImage.builder()
+                .image(image)
+                .build();
+    }
 }
