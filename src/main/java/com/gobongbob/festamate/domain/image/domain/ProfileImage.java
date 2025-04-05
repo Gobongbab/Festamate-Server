f190112f7e018d01ed5ab0fd3d5f6902aa4e1c1f
jae woo Jung <awfjol2008@gmail.com>
2025-04-05 17:39:37 +0900

    feat: ProfileImage 엔티티에서 회원 필드 제거

--- src/main/java/com/gobongbob/festamate/domain/image/domain/ProfileImage.java
+++ src/main/java/com/gobongbob/festamate/domain/image/domain/ProfileImage.java
@@ -1,14 +1,10 @@
 package com.gobongbob.festamate.domain.image.domain;
 
-import com.gobongbob.festamate.domain.member.domain.Member;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,10 +25,6 @@ public class ProfileImage {
     @Embedded
     private Image image;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
-
     @Transient
     private static final ProfileImage DEFAULT_PROFILE_IMAGE = ProfileImage.builder()
             .image(Image.builder()
@@ -40,15 +32,9 @@ public class ProfileImage {
                     .storeName("default_profile_image.png")
                     .uploadName("default_profile_image.png")
                     .build())
-            .member(null) // 기본 이미지라 특정 유저와 연결되지 않음
             .build();
 
     public static ProfileImage getDefaultProfileImage() {
         return DEFAULT_PROFILE_IMAGE;
-    }
-
-    // 연관관계 편의 메서드
-    public void setMember(Member member) {
-        this.member = member;
     }
 }
