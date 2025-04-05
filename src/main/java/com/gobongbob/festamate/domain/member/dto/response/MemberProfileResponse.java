4721a945e47a7b008dcfd32398cc1a6c7f39a0cf
jae woo Jung <awfjol2008@gmail.com>
2025-04-05 22:17:41 +0900

    feat: 프로필 조회 API가 프로필 이미지 및 잔여 티켓 수량을 응답하도록 DTO 수정

--- src/main/java/com/gobongbob/festamate/domain/member/dto/response/MemberProfileResponse.java
+++ src/main/java/com/gobongbob/festamate/domain/member/dto/response/MemberProfileResponse.java
@@ -1,5 +1,7 @@
 package com.gobongbob.festamate.domain.member.dto.response;
 
+import com.gobongbob.festamate.domain.image.domain.Image;
+import com.gobongbob.festamate.domain.image.dto.response.ImageResponse;
 import com.gobongbob.festamate.domain.member.domain.Member;
 
 public record MemberProfileResponse(
@@ -8,17 +10,25 @@ public record MemberProfileResponse(
         String studentId,
         String phoneNumber,
         String gender,
-        String major
+        String major,
+        int maximumTicket,
+        int remainingTicket,
+        ImageResponse profileImage
 ) {
 
     public static MemberProfileResponse fromEntity(Member member) {
+        Image profileImage = member.getProfileImage().getImage();
+
         return new MemberProfileResponse(
                 member.getName(),
                 member.getNickname(),
                 member.getStudentId(),
                 member.getPhoneNumber(),
                 member.getGender() != null ? member.getGender().getName() : "Unknown",
-                member.getMajor() != null ? member.getMajor().getDepartment() : "Unknown"
+                member.getMajor() != null ? member.getMajor().getDepartment() : "Unknown",
+                member.getMaximumTicket(),
+                member.getRemainingTicket(),
+                ImageResponse.fromEntity(profileImage)
         );
     }
 }
