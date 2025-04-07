faf614f5ff85975fa4674cb6aae565a5eff6e701
ahyeonkong <kimah0403@gmail.com>
2025-04-07 11:39:16 +0900

    feat: 관리자 서비스 생성

--- src/main/java/com/gobongbob/festamate/domain/member/application/MemberService.java
+++ src/main/java/com/gobongbob/festamate/domain/member/application/MemberService.java
@@ -1,5 +1,6 @@
 package com.gobongbob.festamate.domain.member.application;
 
+import com.gobongbob.festamate.domain.auth.jwt.domain.CustomMemberDetails;
 import com.gobongbob.festamate.domain.member.domain.Member;
 import com.gobongbob.festamate.domain.member.dto.request.MemberCreateRequest;
 import com.gobongbob.festamate.domain.member.dto.request.ProfileRegisterRequest;
@@ -33,7 +34,21 @@ public class MemberService {
                 .toList();
     }
 
+    // 유저 조회
     public MemberResponse findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .map(MemberResponse::fromEntity)
+                .orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+    }
+
+    // 관리자용 유저 조회
+    public MemberResponse findMemberByIdForAdmin(CustomMemberDetails memberDetails, Long memberId) {
+        String role = memberDetails.getMember().getRole();
+
+        if (!"ADMIN".equals(role)) {
+            throw new IllegalArgumentException("관리자 권한이 필요합니다.");
+        }
+
         return memberRepository.findById(memberId)
                 .map(MemberResponse::fromEntity)
                 .orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
@@ -66,6 +81,25 @@ public class MemberService {
          */
 
         memberRepository.delete(member);
+    }
+
+
+    // 유저 제재(admin)
+    @Transactional
+    public void blockMemberById(Long userId) {
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+        member.block();
+        memberRepository.save(member);
+    }
+
+    // 유저 제재 해제(admin)
+    @Transactional
+    public void unblockMemberById(Long userId) {
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+        member.unblock();
+        memberRepository.save(member);
     }
 
     // 프로필 등록 API
