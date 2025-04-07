10c78945fdfb9b42add9750a5fb0fef33eccdb12
jae woo Jung <awfjol2008@gmail.com>
2025-04-07 19:43:30 +0900

    fix: ProfileImageRepository의 조회 쿼리 메서드 수정

--- src/main/java/com/gobongbob/festamate/domain/image/persistence/ProfileImageRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/image/persistence/ProfileImageRepository.java
@@ -3,8 +3,10 @@ package com.gobongbob.festamate.domain.image.persistence;
 import com.gobongbob.festamate.domain.image.domain.ProfileImage;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
 
+    @Query("select p from ProfileImage p where p.image.storeName = ?1")
     Optional<ProfileImage> findByStoreName(String storeName);
 }
