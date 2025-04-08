e3628b7bee4eb51802d6a858372bb3401e4cb8b4
ahyeonkong <kimah0403@gmail.com>
2025-04-08 23:49:43 +0900

    feat: 문자 인증 관련 필드에 @Getter 추가

--- src/main/java/com/gobongbob/festamate/domain/member/dto/request/ProfileRegisterRequest.java
+++ src/main/java/com/gobongbob/festamate/domain/member/dto/request/ProfileRegisterRequest.java
@@ -3,13 +3,17 @@ package com.gobongbob.festamate.domain.member.dto.request;
 import com.gobongbob.festamate.domain.major.domain.Major;
 import com.gobongbob.festamate.domain.member.domain.Gender;
 import com.gobongbob.festamate.domain.member.domain.Member;
+import lombok.Getter;
 
 // 프로필 등록용 DTO
 public record ProfileRegisterRequest(
         String name,
         String nickname,
         String studentId,
+        @Getter
         String phoneNumber,
+        @Getter
+        String phoneVerificationCodes,
         String gender,
         String college,
         String department
