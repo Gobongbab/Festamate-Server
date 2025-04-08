7f96956e7ec7c2df5bf5211c279174347e376030
ahyeonkong <kimah0403@gmail.com>
2025-04-09 00:34:20 +0900

    fix: 시큐리티 충돌 해결 2

--- src/main/java/com/gobongbob/festamate/domain/auth/oauth/presentation/OauthController.java
+++ src/main/java/com/gobongbob/festamate/domain/auth/oauth/presentation/OauthController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
