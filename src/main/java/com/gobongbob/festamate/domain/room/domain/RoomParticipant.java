12637eaa62ddefbd2c0e7a31b3dae419cc7694f5
jae woo Jung <awfjol2008@gmail.com>
2025-04-09 18:02:17 +0900

    feat: RoomParticipant 엔티티에 role 필드 추가

--- src/main/java/com/gobongbob/festamate/domain/room/domain/RoomParticipant.java
+++ src/main/java/com/gobongbob/festamate/domain/room/domain/RoomParticipant.java
@@ -2,6 +2,8 @@ package com.gobongbob.festamate.domain.room.domain;
 
 import com.gobongbob.festamate.domain.member.domain.Member;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,6 +34,9 @@ public class RoomParticipant {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     private boolean isHost;
 
