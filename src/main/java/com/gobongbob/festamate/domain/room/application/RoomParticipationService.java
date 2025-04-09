af9cf278e55713cab43d3f9231af7d298bed4285
jae woo Jung <awfjol2008@gmail.com>
2025-04-09 18:22:30 +0900

    feat: 모임방 참여 정책 변경에 따른 방 정원 검증 로직 수정

--- src/main/java/com/gobongbob/festamate/domain/room/application/RoomParticipationService.java
+++ src/main/java/com/gobongbob/festamate/domain/room/application/RoomParticipationService.java
@@ -1,17 +1,24 @@
 package com.gobongbob.festamate.domain.room.application;
 
+import static com.gobongbob.festamate.global.response.ResponseCode.ALREADY_PARTICIPATING;
+import static com.gobongbob.festamate.global.response.ResponseCode.MUST_NORMAL;
+import static com.gobongbob.festamate.global.response.ResponseCode.NOT_FOUND_ROOM;
+import static com.gobongbob.festamate.global.response.ResponseCode.NO_PARTICIPATING_ROOM;
+
 import com.gobongbob.festamate.domain.member.domain.Member;
 import com.gobongbob.festamate.domain.member.persistence.MemberRepository;
+import com.gobongbob.festamate.domain.room.domain.Role;
 import com.gobongbob.festamate.domain.room.domain.Room;
 import com.gobongbob.festamate.domain.room.domain.RoomParticipant;
+import com.gobongbob.festamate.domain.room.dto.request.ParticipationWithFriendRequest;
 import com.gobongbob.festamate.domain.room.persistence.RoomRepository;
 import com.gobongbob.festamate.domain.room.presentation.RoomParticipantRepository;
 import com.gobongbob.festamate.global.response.exception.BadRequestException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import static com.gobongbob.festamate.global.response.ResponseCode.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -28,7 +35,7 @@ public class RoomParticipationService {
                 .orElseThrow(() -> new BadRequestException(NOT_FOUND_ROOM));
 
 //        validateRoomParticipation(member.getId());
-        validateRoomFull(room.getId());
+        validateRoomFull(room.getId(), 1);
 
         RoomParticipant roomParticipant = RoomParticipant.createParticipant(room, member, Role.HOST);
         roomParticipantRepository.save(roomParticipant);
@@ -59,14 +66,19 @@ public class RoomParticipationService {
                 });
     }
 
-    private void validateRoomFull(Long roomId) {
+    private void validateRoomFull(Long roomId, int membersToParticipate) {
         Room room = roomRepository.findById(roomId)
                 .orElseThrow(() -> new BadRequestException(NOT_FOUND_ROOM));
-        int participantsCount = roomParticipantRepository.countByRoom_Id(roomId);
 
-        if (room.getMaxParticipants() == participantsCount) {
-            throw new BadRequestException(FULL_ROOM);
+        if (isRoomFull(membersToParticipate, room)) {
+            throw new BadRequestException(NOT_FOUND_ROOM);
         }
+    }
+
+    private boolean isRoomFull(int membersToParticipate, Room room) {
+        int currentParticipants = roomParticipantRepository.countByRoom_Id(room.getId());
+
+        return membersToParticipate + currentParticipants > room.getMaxParticipants();
     }
 
     private void validateNotHost(Room room, Member member) {
