adec6ce6ce362199f3c91adec4d394e22afc9583
jae woo Jung <awfjol2008@gmail.com>
2025-04-15 22:59:02 +0900

    feat: 모임방 관련 QueryDsl Repository 인터페이스 구현

---
+++ src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomQueryDslRepository.java
@@ -0,0 +1,11 @@
+package com.gobongbob.festamate.domain.room.persistence;
+
+import com.gobongbob.festamate.domain.room.domain.Room;
+import com.gobongbob.festamate.domain.room.dto.request.SearchCondition;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+public interface RoomQueryDslRepository {
+
+    Slice<Room> findByFilter(SearchCondition searchCondition, Pageable pageable);
+}
