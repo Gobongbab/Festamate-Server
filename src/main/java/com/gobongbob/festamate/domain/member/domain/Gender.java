62cfff85757766b3a2f1ca44917a7af5e9ea72e1
jae woo Jung <awfjol2008@gmail.com>
2025-04-15 17:44:38 +0900

    refactor: Enum 엔티티에 Getter 및 AllArgsConstructor 어노테이션 추가

--- src/main/java/com/gobongbob/festamate/domain/member/domain/Gender.java
+++ src/main/java/com/gobongbob/festamate/domain/member/domain/Gender.java
@@ -1,23 +1,20 @@
 package com.gobongbob.festamate.domain.member.domain;
 
-import java.util.Arrays;
-
-import com.gobongbob.festamate.global.response.exception.BadRequestException;
-import lombok.Getter;
-
 import static com.gobongbob.festamate.global.response.ResponseCode.NO_GENDER;
 
+import com.gobongbob.festamate.global.response.exception.BadRequestException;
+import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 @Getter
+@AllArgsConstructor
 public enum Gender {
     MALE("남성"),
     FEMALE("여성"),
     ANY("무관");
 
     private final String name;
-
-    Gender(String name) {
-        this.name = name;
-    }
 
     public static Gender findByName(String name) {
         return Arrays.stream(Gender.values())
