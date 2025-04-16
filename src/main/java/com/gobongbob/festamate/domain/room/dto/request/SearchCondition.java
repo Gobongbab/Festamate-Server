15fd2479f5eff07d4af4cec1efc9ccd83c2763a7
jae woo Jung <awfjol2008@gmail.com>
2025-04-17 01:25:15 +0900

    style: 필터에 따른 모임방 조회 API 관련 Swagger 관련 어노테이션 설명 수정

--- src/main/java/com/gobongbob/festamate/domain/room/dto/request/SearchCondition.java
+++ src/main/java/com/gobongbob/festamate/domain/room/dto/request/SearchCondition.java
@@ -2,12 +2,9 @@ package com.gobongbob.festamate.domain.room.dto.request;
 
 import com.gobongbob.festamate.domain.member.domain.Gender;
 import com.gobongbob.festamate.domain.room.domain.Status;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 public record SearchCondition(
-        @Schema(description = "매칭 상태", enumAsRef = true)
         Status status,
-        @Schema(description = "입장 가능 성별", enumAsRef = true)
         Gender gender,
         Integer participants,
         String studentId
