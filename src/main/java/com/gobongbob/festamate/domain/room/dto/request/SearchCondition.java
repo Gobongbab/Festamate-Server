55e9a531278068aacdf72bde612ca9b074a97044
jae woo Jung <awfjol2008@gmail.com>
2025-04-17 02:24:19 +0900

    feat: 학번 범위 조건을 통한 모임방 필터링 로직 추가

--- src/main/java/com/gobongbob/festamate/domain/room/dto/request/SearchCondition.java
+++ src/main/java/com/gobongbob/festamate/domain/room/dto/request/SearchCondition.java
@@ -6,6 +6,8 @@ import com.gobongbob.festamate.domain.room.domain.Status;
 public record SearchCondition(
         Status status,
         Gender gender,
+        String minStudentId,
+        String maxStudentId,
         Integer participants,
         String studentId
 ) {
