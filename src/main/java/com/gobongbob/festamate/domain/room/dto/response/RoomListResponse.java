d7669e949e033d42c131cd6b75bfaf83ba454065
jae woo Jung <awfjol2008@gmail.com>
2025-04-16 19:22:30 +0900

    fix: 모임방 응답 DTO의 시간이 올바로 출력되지 않는 오류 수정

--- src/main/java/com/gobongbob/festamate/domain/room/dto/response/RoomListResponse.java
+++ src/main/java/com/gobongbob/festamate/domain/room/dto/response/RoomListResponse.java
@@ -1,5 +1,6 @@
 package com.gobongbob.festamate.domain.room.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gobongbob.festamate.domain.image.domain.Image;
 import com.gobongbob.festamate.domain.image.dto.response.ImageResponse;
 import com.gobongbob.festamate.domain.room.domain.Room;
@@ -11,6 +12,7 @@ public record RoomListResponse(
         String place,
         String content,
         String preferredGender,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime meetingDateTime,
         int maxParticipants,
         int currentParticipants,
