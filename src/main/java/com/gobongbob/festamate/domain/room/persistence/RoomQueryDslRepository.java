ad1006cf53abe85b4c92331d8017dd80acd69d22
jae woo Jung <awfjol2008@gmail.com>
2025-04-16 15:23:21 +0900

    feat: 필터링에 따른 모임방 조회 기능 Service 단에 적용

--- src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomQueryDslRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomQueryDslRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.domain.Slice;
 
 public interface RoomQueryDslRepository {
 
-    Slice<Room> findBySearchCondition(SearchCondition searchCondition, Pageable pageable);
+    Slice<Room> findBySearchCondition(Pageable pageable, SearchCondition searchCondition);
 }
