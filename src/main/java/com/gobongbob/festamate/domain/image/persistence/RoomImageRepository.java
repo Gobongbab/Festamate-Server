8b5f91e5b920fc21cb126816600ecc92359884ac
jae woo Jung <awfjol2008@gmail.com>
2025-04-17 18:45:58 +0900

    feat: RoomImageRepository 구현

---
+++ src/main/java/com/gobongbob/festamate/domain/image/persistence/RoomImageRepository.java
@@ -0,0 +1,8 @@
+package com.gobongbob.festamate.domain.image.persistence;
+
+import com.gobongbob.festamate.domain.image.domain.RoomImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
+
+}
