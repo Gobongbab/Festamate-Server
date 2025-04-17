d9e3359ee7403bb6b53d86676a0e9c7449d5b1be
jae woo Jung <awfjol2008@gmail.com>
2025-04-17 13:38:43 +0900

    refactor: CouponController 내부 스웨거 관련 어노테이션 분리

--- src/main/java/com/gobongbob/festamate/domain/coupon/presentation/CouponController.java
+++ src/main/java/com/gobongbob/festamate/domain/coupon/presentation/CouponController.java
@@ -4,11 +4,6 @@ import com.gobongbob.festamate.domain.auth.jwt.domain.CustomMemberDetails;
 import com.gobongbob.festamate.domain.coupon.application.CouponService;
 import com.gobongbob.festamate.domain.coupon.dto.request.UseCouponRequest;
 import com.gobongbob.festamate.global.response.SuccessResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,21 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/coupons")
-@Tag(name = "Coupon", description = "쿠폰 관련 API")
-public class CouponController {
+public class CouponController implements CouponApi {
 
     private final CouponService couponService;
 
-    @Operation(summary = "쿠폰 사용", description = "쿠폰을 사용합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "400", description = "유효하지 않은 쿠폰입니다.")
-    })
+    @Override
     @PostMapping("")
     public SuccessResponse<Void> useCoupon(
-            @Parameter(description = "인증된 사용자 정보", hidden = true)
             @AuthenticationPrincipal CustomMemberDetails memberDetails,
-            @Parameter(description = "쿠폰 사용 요청 정보")
             @RequestBody UseCouponRequest request
     ) {
         couponService.useCoupon(memberDetails.getMember(), request);
@@ -41,13 +29,9 @@ public class CouponController {
         return new SuccessResponse<>();
     }
 
-    @Operation(summary = "쿠폰 초기화", description = "사용자의 쿠폰을 초기화합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
-    })
+    @Override
     @PostMapping("/init")
     public SuccessResponse<Void> initializeCoupons(
-            @Parameter(description = "인증된 사용자 정보", hidden = true)
             @AuthenticationPrincipal CustomMemberDetails memberDetails
     ) {
         couponService.initializeCoupons(memberDetails.getMember());
