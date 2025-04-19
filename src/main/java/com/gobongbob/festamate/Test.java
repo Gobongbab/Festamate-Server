909df5b2da89d3a3e3e0c1a62f54c8a392a6784e
Muokok <sgo5178@kyonggi.ac.kr>
2025-04-20 00:59:25 +0900

    fix: healtg check URI 수정

--- src/main/java/com/gobongbob/festamate/Test.java
+++ src/main/java/com/gobongbob/festamate/Test.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class Test {
 
-  @GetMapping("/healthcheck")
+  @GetMapping("/health")
   public ResponseEntity<String> healthCheck() {
     return ResponseEntity.ok("Health Check");
     // test
