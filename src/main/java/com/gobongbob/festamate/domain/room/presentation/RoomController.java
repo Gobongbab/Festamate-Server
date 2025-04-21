13daa10419317609e851c26b7bd806c29094707c
jae woo Jung <awfjol2008@gmail.com>
2025-04-22 05:17:11 +0900

    fix: RoonController 내의 import문 와일드카드 제거

--- src/main/java/com/gobongbob/festamate/domain/room/presentation/RoomController.java
+++ src/main/java/com/gobongbob/festamate/domain/room/presentation/RoomController.java
@@ -23,7 +23,16 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
