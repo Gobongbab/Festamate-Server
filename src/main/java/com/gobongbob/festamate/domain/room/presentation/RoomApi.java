1356bfc699dd7b62f179ee16bebbd84f5411d4b4
jae woo Jung <awfjol2008@gmail.com>
2025-04-22 04:40:49 +0900

    fix: RoomApi 내 RequestBody import문 수정

--- src/main/java/com/gobongbob/festamate/domain/room/presentation/RoomApi.java
+++ src/main/java/com/gobongbob/festamate/domain/room/presentation/RoomApi.java
@@ -11,6 +11,7 @@ import com.gobongbob.festamate.domain.room.dto.response.RoomResponse;
 import com.gobongbob.festamate.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,7 +28,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
