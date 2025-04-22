05689f2fb02346b8b4f70040706ff20eb3dd82d4
jae woo Jung <awfjol2008@gmail.com>
2025-04-22 22:38:21 +0900

    feat: 모임방 권한 관련 Enum 이름값 변경

--- src/main/java/com/gobongbob/festamate/domain/room/domain/RoomAuthority.java
+++ src/main/java/com/gobongbob/festamate/domain/room/domain/RoomAuthority.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum RoomAuthority {
     HOST("호스트"),
-    PARTICIPANT("참여자"),
-    NON_PARTICIPANT("비참여 회원"),
+    PARTICIPANT("참여 회원"),
+    NON_PARTICIPANT("미참여 회원"),
     NON_MEMBER("비회원");
 
     private final String name;
