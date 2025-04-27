b8e32d560ee999889f8d19817e9799788a23988f
Muokok <sgo5178@kyonggi.ac.kr>
2025-04-27 18:57:27 +0900

    refactor: 리플리케이션 임시 비활성화

--- src/main/java/com/gobongbob/festamate/global/ReplicationRoutingDataSource.java
+++ src/main/java/com/gobongbob/festamate/global/ReplicationRoutingDataSource.java
@@ -10,19 +10,20 @@ public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
     // 현재 사용할 DataSource의 키(lookup key)를 결정하는 핵심 메소드. Spring의 트랜잭션 관리자가 호출합니다.
     @Override
     protected Object determineCurrentLookupKey() {
-        // TransactionSynchronizationManager를 사용하여 현재 실행 중인 스레드에 활성화된 트랜잭션이 읽기 전용(read-only)인지 확인합니다.
-        // @Transactional(readOnly = true) 가 적용된 서비스 메소드가 호출되면 true가 됩니다.
-        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
-
-        // 만약 현재 트랜잭션이 읽기 전용이라면
-        if (isReadOnly) {
-            // "slave" 라는 문자열(lookup key)을 반환합니다.
-            // AbstractRoutingDataSource는 이 키를 사용하여 targetDataSources 맵에서 실제 사용할 DataSource (replicaDataSource)를 찾습니다.
-            log.info("Routing to SLAVE data source");
-            return "slave";
-        } else {
-            log.info("Routing to MASTER data source");
-            return "master";
-        }
+//        // TransactionSynchronizationManager를 사용하여 현재 실행 중인 스레드에 활성화된 트랜잭션이 읽기 전용(read-only)인지 확인합니다.
+//        // @Transactional(readOnly = true) 가 적용된 서비스 메소드가 호출되면 true가 됩니다.
+//        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+//
+//        // 만약 현재 트랜잭션이 읽기 전용이라면
+//        if (isReadOnly) {
+//            // "slave" 라는 문자열(lookup key)을 반환합니다.
+//            // AbstractRoutingDataSource는 이 키를 사용하여 targetDataSources 맵에서 실제 사용할 DataSource (replicaDataSource)를 찾습니다.
+//            log.info("Routing to SLAVE data source");
+//            return "slave";
+//        } else {
+//            log.info("Routing to MASTER data source");
+//            return "master";
+//        }
+        return "master"; // 기본적으로 master로 설정하여 리플리케이션 비활성화
     }
 }
