1e178875705c495b898670fb7c4f7323ce40f101
jae woo Jung <awfjol2008@gmail.com>
2025-04-28 23:39:34 +0900

    feat: 웹소켓 연결 엔드포인트에 SockJs 옵션 추가

--- src/main/java/com/gobongbob/festamate/global/config/WebSocketConfig.java
+++ src/main/java/com/gobongbob/festamate/global/config/WebSocketConfig.java
@@ -30,6 +30,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+
+        registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*");
 
         registry.setErrorHandler(chatErrorHandler);
