d33786b5b5c23f888586a63f46a323193009afbf
ahyeonkong <kimah0403@gmail.com>
2025-04-29 19:56:05 +0900

    refactor: 관리자 신고 API 엔드포인트 경로를 `/api/admin/report`로 수정

--- src/main/java/com/gobongbob/festamate/domain/admin/presentation/AdminController.java
+++ src/main/java/com/gobongbob/festamate/domain/admin/presentation/AdminController.java
@@ -5,8 +5,11 @@ import com.gobongbob.festamate.domain.auth.jwt.domain.CustomMemberDetails;
 import com.gobongbob.festamate.domain.auth.jwt.dto.request.LoginRequest;
 import com.gobongbob.festamate.domain.member.application.MemberService;
 import com.gobongbob.festamate.domain.member.dto.response.MemberResponse;
+import com.gobongbob.festamate.domain.report.application.ReportService;
+import com.gobongbob.festamate.domain.report.dto.response.ReportRoomResponse;
 import com.gobongbob.festamate.domain.room.application.RoomService;
 import com.gobongbob.festamate.global.response.SuccessResponse;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -26,7 +29,9 @@ public class AdminController implements AdminApi {
     private final MemberService memberService;
     private final RoomService roomService;
     private final AdminService adminService;
+    private final ReportService reportService;
 
+    // 관리자용 유저 목록 조회
     @Override
     @GetMapping("/users/{userId}")
     public SuccessResponse<MemberResponse> getUserById(
@@ -36,6 +41,7 @@ public class AdminController implements AdminApi {
         return new SuccessResponse<>(memberService.findMemberByIdForAdmin(memberDetails, userId));
     }
 
+    // 관리자용 모임방 삭제
     @Override
     @DeleteMapping("/rooms/{roomId}")
     public SuccessResponse<Void> deleteRoomById(
@@ -46,6 +52,7 @@ public class AdminController implements AdminApi {
         return new SuccessResponse<>();
     }
 
+    // 관리자용 유저 제재
     @Override
     @PostMapping("/users/{userId}/block")
     public SuccessResponse<Void> blockUser(
@@ -56,6 +63,7 @@ public class AdminController implements AdminApi {
         return new SuccessResponse<>();
     }
 
+    // 관리자용 유저 제재 해제
     @Override
     @PostMapping("/users/{userId}/unblock")
     public SuccessResponse<Void> unblockUser(
@@ -66,7 +74,7 @@ public class AdminController implements AdminApi {
         return new SuccessResponse<>();
     }
 
-    // 관리자 로그인
+    // 관리자용 일반 로그인
     @Override
     @PostMapping("/login")
     public SuccessResponse<Map<String, String>> loginAdmin(
@@ -75,5 +83,23 @@ public class AdminController implements AdminApi {
     ) {
         Map<String, String> tokens = adminService.loginAdmin(loginRequest);
         return new SuccessResponse<>(tokens);
+    }
+
+    // 관리자용 모든 신고 조회
+    @Override
+    @GetMapping("/report")
+    public SuccessResponse<List<ReportRoomResponse>> getAllReports(
+            @AuthenticationPrincipal CustomMemberDetails memberDetails
+    ) {
+        return new SuccessResponse<>(reportService.getAllReports());
+    }
+
+    // 관리자용 미처리 신고 조회
+    @Override
+    @GetMapping("/report/unprocessed")
+    public SuccessResponse<List<ReportRoomResponse>> getUnprocessedReports(
+            @AuthenticationPrincipal CustomMemberDetails memberDetails
+    ) {
+        return new SuccessResponse<>(reportService.getUnprocessedReports());
     }
 }
--- src/main/java/com/gobongbob/festamate/domain/report/presentation/ReportController.java
+++ src/main/java/com/gobongbob/festamate/domain/report/presentation/ReportController.java
@@ -4,14 +4,11 @@ import com.gobongbob.festamate.domain.auth.jwt.domain.CustomMemberDetails;
 import com.gobongbob.festamate.domain.report.application.ReportService;
 import com.gobongbob.festamate.domain.report.dto.request.ReportMemberRequest;
 import com.gobongbob.festamate.domain.report.dto.request.ReportRoomRequest;
-import com.gobongbob.festamate.domain.report.dto.response.ReportRoomResponse;
 import com.gobongbob.festamate.global.response.SuccessResponse;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -48,23 +45,5 @@ public class ReportController implements ReportApi {
     ) {
         reportService.reportMember(memberDetails.getMember(), memberId, request);
         return new SuccessResponse<>();
-    }
-
-    // 모든 신고 조회
-    @Override
-    @GetMapping("")
-    public SuccessResponse<List<ReportRoomResponse>> getAllReports(
-            @AuthenticationPrincipal CustomMemberDetails memberDetails
-    ) {
-        return new SuccessResponse<>(reportService.getAllReports());
-    }
-
-    // 미처리 신고 조회
-    @Override
-    @GetMapping("/unprocessed")
-    public SuccessResponse<List<ReportRoomResponse>> getUnprocessedReports(
-            @AuthenticationPrincipal CustomMemberDetails memberDetails
-    ) {
-        return new SuccessResponse<>(reportService.getUnprocessedReports());
     }
 }
