d861863b3df3cf830af3e3c31d34103bea8c6473
ahyeonkong <kimah0403@gmail.com>
2025-04-29 16:08:29 +0900

    refactor: 프로필 사진 수정 API 스웨거 문서 개선

--- src/main/java/com/gobongbob/festamate/domain/member/presentation/MemberApi.java
+++ src/main/java/com/gobongbob/festamate/domain/member/presentation/MemberApi.java
@@ -9,10 +9,12 @@ import com.gobongbob.festamate.domain.room.dto.response.MemberExistResponse;
 import com.gobongbob.festamate.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import java.util.List;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -69,9 +71,15 @@ public interface MemberApi {
     );
 
     // 나의 프로필 사진 수정
+    @Operation(summary = "나의 프로필 사진 수정", description = "나의 프로필 사진을 수정합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
     @PutMapping("/api/auth/members/profile/photo")
     SuccessResponse<Void> updateProfilePhoto(
             @AuthenticationPrincipal CustomMemberDetails memberDetails,
+            @Parameter(description = "프로필 이미지 (단일 파일)",
+                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
             @RequestPart("profileImage") MultipartFile profileImage
     );
 
