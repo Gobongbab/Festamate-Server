c5385e5aff701bc6705474dda5896069d4036dbe
jae woo Jung <awfjol2008@gmail.com>
2025-05-01 04:27:56 +0900

    test: CustomMemberDetails에 정의된 hascode 수정

--- src/main/java/com/gobongbob/festamate/domain/auth/jwt/domain/CustomMemberDetails.java
+++ src/main/java/com/gobongbob/festamate/domain/auth/jwt/domain/CustomMemberDetails.java
@@ -81,7 +81,8 @@ public class CustomMemberDetails implements UserDetails, Serializable, Principal
 
     @Override
     public int hashCode() {
-        return Objects.hash(member.getId());
+        return member != null && member.getId() != null
+                ? Objects.hash(member.getId())
+                : 0; // 또는 throw new IllegalStateException("member or member.id is null");
     }
-
 }
