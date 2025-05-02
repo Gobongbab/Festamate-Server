f2cef4aadf2bc4ebea7acd89c329e74ad91280be
jae woo Jung <awfjol2008@gmail.com>
2025-05-02 20:45:42 +0900

    feat: 회원 id를 통한 모임 및 채팅방 조회 메서드 작성

--- src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomRepository.java
@@ -1,6 +1,7 @@
 package com.gobongbob.festamate.domain.room.persistence;
 
 import com.gobongbob.festamate.domain.room.domain.Room;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,4 +20,13 @@ public interface RoomRepository extends Repository<Room, Long>, RoomQueryDslRepo
     Optional<Room> findByIdWithHost(Long id);
 
     void delete(Room room);
+
+    @Query("""
+                SELECT DISTINCT r
+                FROM RoomParticipant rp
+                JOIN rp.room r
+                JOIN FETCH r.chatRoom
+                WHERE rp.member.id = :memberId
+            """)
+    List<Room> findRoomsWithChatRoomByMember(Long memberId);
 }
