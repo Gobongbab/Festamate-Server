f91288ad91840fd072a574f34b09432bf3d34612
Muokok <sgo5178@kyonggi.ac.kr>
2025-05-03 01:16:44 +0900

    refactor: 모임방 내 모든 인원에게 매칭 완료 알림 보내도록 수정

--- src/main/java/com/gobongbob/festamate/domain/room/application/RoomParticipationService.java
+++ src/main/java/com/gobongbob/festamate/domain/room/application/RoomParticipationService.java
@@ -51,19 +51,23 @@ public class RoomParticipationService {
         participants.forEach(participant -> participateRoom(participant, room));
         if (room.isFull()) { // 방에 참여자가 다 찼을 때
             room.updateStatus(Status.MATCHED);
+
+            // **방 매칭 시 FCM 알림 전송**
+            room.getParticipants().forEach(participant -> {
+                String fcmToken = participant.getMember().getFcmToken(); // FCM 토큰 가져오기
+                Long participantId = participant.getMember().getId();
+                if (fcmToken != null) {
+                    notificationService.sendNotification(
+                            fcmToken,
+                            "방 매칭 완료",
+                            "방 매칭이 완료되었습니다: " + room.getTitle(),
+                            participantId
+                    );
+                }
+            });
         }
       
-        // **방 매칭 시 FCM 알림 전송**
-        participants.forEach(participant -> {
-            String fcmToken = participant.getFcmToken();  // FCM 토큰 가져오기
-            if (fcmToken != null) {
-                notificationService.sendNotification(
-                        fcmToken,
-                        "방 매칭 완료",
-                        "방 매칭이 완료되었습니다: " + room.getTitle()
-                );
-            }
-        });
+
 
         return room.getChatRoom();
     }
