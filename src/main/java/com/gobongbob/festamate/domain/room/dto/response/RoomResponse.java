85caea507236d2d3f5680ad23a808388526a8ef7
jae woo Jung <awfjol2008@gmail.com>
2025-05-02 20:47:32 +0900

    feat: 모임 상세 조회 DTO에 채팅방 id 필드 추가

--- src/main/java/com/gobongbob/festamate/domain/room/dto/response/RoomResponse.java
+++ src/main/java/com/gobongbob/festamate/domain/room/dto/response/RoomResponse.java
@@ -25,6 +25,7 @@ public record RoomResponse(
         int maxParticipants,
         int currentParticipants,
         RoomAuthority roomAuthority,
+        Long chatRoomId,
         List<ParticipantResponse> hostParticipants,
         List<ParticipantResponse> guestParticipants,
         List<ImageResponse> images
@@ -49,6 +50,7 @@ public record RoomResponse(
                 room.getMaxParticipants(),
                 room.getParticipants().size(),
                 roomAuthority,
+                room.getChatRoom().getId(),
                 toParticipantResponse(hostParticipants),
                 toParticipantResponse(guestParticipants),
                 toImageResponse(room)
