5a34cd8d81f76d772aed888fddc5fec97f8b2649
jae woo Jung <awfjol2008@gmail.com>
2025-05-09 17:15:56 +0900

    feat: BaseTimeEntity 구현

---
+++ src/main/java/com/gobongbob/festamate/global/entity/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package com.gobongbob.festamate.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
+
+
