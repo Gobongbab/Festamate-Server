1285271f50b5670614fda34e53907067ab17a085
jae woo Jung <awfjol2008@gmail.com>
2025-05-09 21:54:27 +0900

    fix: 모임 조회 시의 학번 필터링 로직 재수정

--- src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomQueryDslRepositoryImpl.java
+++ src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomQueryDslRepositoryImpl.java
@@ -9,8 +9,6 @@ import com.gobongbob.festamate.domain.room.domain.Room;
 import com.gobongbob.festamate.domain.room.domain.Status;
 import com.gobongbob.festamate.domain.room.dto.request.FilteringCondition;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.NumberTemplate;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -116,13 +114,9 @@ public class RoomQueryDslRepositoryImpl implements RoomQueryDslRepository {
 
     private BooleanExpression studentIdContains(String minStudentId, String maxStudentId) {
         if (hasText(minStudentId) && hasText(maxStudentId)) {
-            NumberTemplate<Integer> preferredMin = Expressions.numberTemplate(Integer.class, "CAST({0} AS SIGNED)",
-                    room.preferredStudentIdMin);
-            NumberTemplate<Integer> preferredMax = Expressions.numberTemplate(Integer.class, "CAST({0} AS SIGNED)",
-                    room.preferredStudentIdMax);
 
-            return preferredMin.goe(Integer.parseInt(minStudentId))
-                    .and(preferredMax.loe(Integer.parseInt(maxStudentId)));
+            return room.preferredStudentIdMin.goe(Integer.parseInt(minStudentId))
+                    .and(room.preferredStudentIdMax.loe(Integer.parseInt(maxStudentId)));
         }
 
         return null;
@@ -132,13 +126,9 @@ public class RoomQueryDslRepositoryImpl implements RoomQueryDslRepository {
     private BooleanExpression studentIdContains(String fullStudentId) {
         if (hasText(fullStudentId)) {
             int studentId = Integer.parseInt(fullStudentId.substring(2, 4));
-            NumberTemplate<Integer> preferredMin = Expressions.numberTemplate(Integer.class, "CAST({0} AS SIGNED)",
-                    room.preferredStudentIdMin);
-            NumberTemplate<Integer> preferredMax = Expressions.numberTemplate(Integer.class, "CAST({0} AS SIGNED)",
-                    room.preferredStudentIdMax);
 
             // 모임방의 최소 학번 조건이 25일 경우 24는 통과하고, 최대 학번 조건이 20일 경우 19는 통과하지 못함
-            return preferredMin.goe(studentId).and(preferredMax.loe(studentId));
+            return room.preferredStudentIdMin.goe(studentId).and(room.preferredStudentIdMax.loe(studentId));
         }
 
         return null;
