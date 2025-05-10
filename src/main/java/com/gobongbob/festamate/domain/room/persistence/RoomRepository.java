c3108ea5e70c73b4cc8e638552149fea0c287fff
jae woo Jung <awfjol2008@gmail.com>
2025-05-10 16:32:13 +0900

    feat: 모임 시간이 지난 방을 조회하는 쿼리 메서드 작성

--- src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/room/persistence/RoomRepository.java
@@ -1,6 +1,9 @@
 package com.gobongbob.festamate.domain.room.persistence;
 
 import com.gobongbob.festamate.domain.room.domain.Room;
+import com.gobongbob.festamate.domain.room.domain.Status;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +15,9 @@ public interface RoomRepository extends Repository<Room, Long>, RoomQueryDslRepo
     Room save(Room room);
 
     Page<Room> findAll(Pageable pageable);
+
+    @Query("SELECT r FROM Room r WHERE r.status = ?1 AND r.meetingDateTime > ?2")
+    List<Room> findRoomsByScheduledTimeAfter(Status status, LocalDateTime meetingDateTime);
 
     Optional<Room> findById(Long id);
 
