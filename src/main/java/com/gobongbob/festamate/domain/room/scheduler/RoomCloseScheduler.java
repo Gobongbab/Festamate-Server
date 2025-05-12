ca14f57c8d9ca679ce8cbfc51d017af42826f56f
jae woo Jung <awfjol2008@gmail.com>
2025-05-12 17:48:41 +0900

    feat: 모임 자동 종료 관련 로그 제거

--- src/main/java/com/gobongbob/festamate/domain/room/scheduler/RoomCloseScheduler.java
+++ src/main/java/com/gobongbob/festamate/domain/room/scheduler/RoomCloseScheduler.java
@@ -18,10 +18,7 @@ public class RoomCloseScheduler {
     @Scheduled(fixedRate = 60000) // 1분마다 실행
     public void closeExpiredRooms() {
         List<Room> expiredRooms = roomRepository.findRoomsByScheduledTimeBefore(LocalDateTime.now());
-        expiredRooms.forEach(room -> {
-            room.updateStatus(Status.CLOSED);
-            System.out.println("[스케줄러] 상태 CLOSED로 변경 → roomId: " + room.getId());
-        });
+        expiredRooms.forEach(room -> room.updateStatus(Status.CLOSED));
 
         roomRepository.saveAll(expiredRooms); // 변경된 상태 저장
     }
