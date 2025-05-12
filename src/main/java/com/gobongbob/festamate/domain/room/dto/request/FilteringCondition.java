bcf5f696c018610fb94d5a95821a68549eccf78a
jae woo Jung <awfjol2008@gmail.com>
2025-05-12 20:43:08 +0900

    feat: 모임 검색 시의 필터링 DTO 내에 키워드 필드 추가

--- src/main/java/com/gobongbob/festamate/domain/room/dto/request/FilteringCondition.java
+++ src/main/java/com/gobongbob/festamate/domain/room/dto/request/FilteringCondition.java
@@ -4,6 +4,7 @@ import com.gobongbob.festamate.domain.member.domain.Gender;
 import com.gobongbob.festamate.domain.room.domain.Status;
 
 public record FilteringCondition(
+        String keyword,
         Status status,
         Gender gender,
         String minStudentId,
