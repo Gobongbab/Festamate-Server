507b1d94aa538112fb52e0205434a392c5cc7e29
jae woo Jung <awfjol2008@gmail.com>
2025-05-13 15:01:08 +0900

    feat: 응답 코드 정리 및 신규 코드 추가

--- src/main/java/com/gobongbob/festamate/global/response/ResponseCode.java
+++ src/main/java/com/gobongbob/festamate/global/response/ResponseCode.java
@@ -10,41 +10,52 @@ public enum ResponseCode {
     INTERNAL_SEVER_ERROR(false, "서버 에러가 발생하였습니다. 관리자에게 문의해 주세요."),
     INVALID_REQUEST(false, "올바르지 않은 요청입니다."),
 
-    // 모임방
-    NOT_FOUND_ROOM(false, "모임방이 존재하지 않습니다."),
-    ALREADY_PARTICIPATING(false, "이미 모임방에 참여하고 있습니다."),
+    // 모임 공통
+    NOT_FOUND_ROOM(false, "모임이 존재하지 않습니다."),
     NO_PARTICIPATING_ROOM(false, "참여중인 모임방이 존재하지 않습니다."),
     ALREADY_MATCHED(false, "이미 매칭이 완료된 모임방입니다."),
-    ROOM_NOT_JOINABLE(false, "모임의 남은 자리가 입장 인원 수와 일치하지 않습니다."),
+
+    // 모임 참여 및 생성
     ROOM_FULL(false, "모임이 가득 찼습니다."),
     PHONE_NUMBER_DUPLICATE(false, "같은 회원이 동시에 참여할 수 없습니다."),
+    ROOM_NOT_JOINABLE(false, "모임의 남은 자리가 입장 인원 수와 일치하지 않습니다."),
     GENDER_NOT_MATCH(false, "성별이 일치하지 않습니다."),
     STUDENT_ID_NOT_MATCH(false, "학번이 일치하지 않습니다."),
+    NOT_ENOUGH_TICKET(false, "티켓이 부족합니다."),
+
+    // 모임 수정
     MUST_HOST(false, "방장이어야 합니다."),
     MUST_NORMAL(false, "일반 회원이어야 합니다."),
-    CAN_NOT_UPDATE(false, "방에 방장을 제외한 다른 사용자가 입장한 상태에서는 수정할 수 없습니다."),
-    NOT_ENOUGH_TICKET(false, "티켓이 부족합니다."),
+    ROOM_UPDATE_NOT_AVAILABLE(false, "방에 방장을 제외한 다른 사용자가 입장한 상태에서는 수정할 수 없습니다."),
     ROLE_NOT_FOUND(false, "해당하는 권한이 없습니다."),
     STATUS_NOT_FOUND(false, "해당하는 모임방 상태가 없습니다. (매칭중, 매칭완료, 종료)"),
+
+    // 인증
+    UNEXPECTED_TOKEN(false, "올바르지 않은 토큰입니다."),
 
     // 회원
     NO_MEMBER(false, "사용자가 존재하지 않습니다."),
     NO_GENDER(false, "해당하는 성별이 없습니다."),
+    NO_DEPARTMENT(false, "해당하는 학과가 존재하지 않습니다."),
     DUPLICATE_NICKNAME(false, "중복된 닉네임입니다."),
     DUPLICATE_STUDENT_ID(false, "중복된 학번입니다."),
     USER_NOT_FOUND(false, "해당 유저를 찾을 수 없습니다"),
-    UNEXPECTED_TOKEN(false, "올바르지 않은 토큰입니다."),
     PHONE_NOT_VERIFIED(false, "전화번호 인증이 완료되지 않았습니다."),
-    PHONE_NUMBER_DUPLICATE_AMONG_PARTICIPANTS(false,  "초대된 사용자(호스트 및 친구) 간에 중복된 전화번호가 있습니다."),
+    MEMBER_NOT_FOUND_BY_PHONE_NUMBER(false, "전화번호 [ %s ] 에 해당하는 회원을 찾을 수 없습니다."),
+    PHONE_NUMBER_DUPLICATE_AMONG_PARTICIPANTS(false, "초대된 사용자(호스트 및 친구) 간에 중복된 전화번호가 있습니다."),
 
-    // 파일
+    // 이미지 가공
     EMPTY_FILE(false, "파일이 비어있습니다."),
-    EXCEED_IMAGE_CAPACITY(false, "업로드 가능한 이미지 용량을 초과했습니다."),
+    EXCEED_IMAGE_CAPACITY(false, "이미지 파일 크기는 %dMB 이하여야 합니다."),
+    UNSUPPORTED_IMAGE_EXTENSION(false, "%s는 지원하지 않는 이미지 파일 확장자입니다."),
+    IMAGE_STORE_DTO_CREATE_FAILED(false, "이미지 파일 저장을 위한 DTO 생성에 실패했습니다."),
+
+    // S3 이미지 업로드
+    S3_UPLOAD_FAILED(false, "S3 버킷에 이미지를 업로드하는 중 문제가 발생했습니다."),
 
     // 채팅방
     CHAT_ROOM_NOT_FOUND(false, "채팅방이 존재하지 않습니다."),
     NO_AUTHORITY_CHAT_ROOM(false, "채팅방을 조회할 수 있는 권한이 없습니다."),
-    NO_DEPARTMENT(false, "해당하는 학과가 존재하지 않습니다."),
 
     // 신고
     CAN_NOT_REPORT_MYSELF(false, "자신의 방은 신고할 수 없습니다."),
@@ -57,7 +68,6 @@ public enum ResponseCode {
     NOT_EXIST_TICKET(false, "존재하지 않는 티켓입니다."),
     ALREADY_USED_TICKET(false, "이미 사용된 쿠폰입니다."),
     EXPIRED_TICKET(false, "만료된 쿠폰입니다."),
-
 
     // 관리자
     NO_ADMIN(false, "관리자 권한이 필요합니다.");
