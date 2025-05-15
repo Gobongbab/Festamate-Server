97a430ea371ce5dd617301b622e5a05630bfcaa5
Muokok <sgo5178@kyonggi.ac.kr>
2025-05-15 20:39:27 +0900

    refactor: 닉네임 수정 시 중복 예외 처리

--- src/main/java/com/gobongbob/festamate/domain/member/application/MemberService.java
+++ src/main/java/com/gobongbob/festamate/domain/member/application/MemberService.java
@@ -109,6 +109,9 @@ public class MemberService {
     @Transactional
     @CheckActiveUser
     public void updateMemberProfileById(Member member, ProfileUpdateRequest request) {
+        if(memberRepository.existsByNickname(member.getNickname())){
+            throw new BadRequestException(DUPLICATE_NICKNAME);
+        }
         member.updateProfile(request.nickname());
         memberRepository.save(member);
     }
