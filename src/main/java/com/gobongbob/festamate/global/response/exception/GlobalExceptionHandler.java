5853665dc74f35056f3f90fdc028f43e6469d107
jae woo Jung <awfjol2008@gmail.com>
2025-11-08 17:07:34 +0900

    feat: 전역 예외 핸들러에 500 에러 처리 관련 로직 추가

--- src/main/java/com/gobongbob/festamate/global/response/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gobongbob/festamate/global/response/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.gobongbob.festamate.global.response.exception;
 
+import static com.gobongbob.festamate.global.response.ResponseCode.EXCEED_IMAGE_CAPACITY;
+import static com.gobongbob.festamate.global.response.ResponseCode.INVALID_REQUEST;
+
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.springframework.http.HttpHeaders;
@@ -11,15 +15,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Objects;
-
-import static com.gobongbob.festamate.global.response.ResponseCode.EXCEED_IMAGE_CAPACITY;
-import static com.gobongbob.festamate.global.response.ResponseCode.INVALID_REQUEST;
-
 /**
- * 전역 예외 처리기입니다.
- * body로 클라이언트에게 유용한 피드백을 제공하면서, 로깅을 추가하여 서버 측에서도 문제를 효과적으로 추적할 수 있게 해줍니다.
- * */
+ * 전역 예외 처리기입니다. body로 클라이언트에게 유용한 피드백을 제공하면서, 로깅을 추가하여 서버 측에서도 문제를 효과적으로 추적할 수 있게 해줍니다.
+ */
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -54,7 +52,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(new ExceptionResponse(EXCEED_IMAGE_CAPACITY.isSuccess(), message));
     }
 
-     // 일반적인 잘못된 요청 예외를 처리합니다.
+    // 일반적인 잘못된 요청 예외를 처리합니다.
     @ExceptionHandler(BadRequestException.class) // BadRequestException 클래스의 예외가 발생했을 때 이 메소드가 처리함을 명시
     public ResponseEntity<ExceptionResponse> handleBadRequestException(BadRequestException e) {
         log.warn(e.getMessage(), e);
@@ -63,4 +61,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(new ExceptionResponse(INVALID_REQUEST.isSuccess(), e.getMessage()));
     }
 
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ExceptionResponse> handleInternalServerError(Exception e) {
+        // 500 에러는 서버의 버그이므로, WARN이 아닌 ERROR 레벨로 로깅해야 합니다.
+        log.error("처리되지 못한 서버 내부 오류 발생: {}", e.getMessage(), e);
+
+        // 클라이언트에게는 상세한 오류 내용을 숨기고, 공통 응답 포맷을 반환합니다.
+        return ResponseEntity
+                .internalServerError() // HTTP 500
+                .body(new ExceptionResponse(
+                        false, // isSuccess
+                        "서버 내부 오류가 발생했습니다. 관리자에게 문의하세요." // 클라이언트에게 노출할 메시지
+                ));
+    }
 }
