c1070511e48efc248d2a281abf3ddc44062568f8
jae woo Jung <awfjol2008@gmail.com>
2026-01-23 19:15:17 +0900

    feat: 참여중인 채팅 조회 쿼리 작성

--- src/main/java/com/gobongbob/festamate/domain/chat/persistence/ChatRoomRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/chat/persistence/ChatRoomRepository.java
@@ -3,6 +3,8 @@ package com.gobongbob.festamate.domain.chat.persistence;
 import com.gobongbob.festamate.domain.chat.domain.ChatRoom;
 import com.gobongbob.festamate.domain.room.domain.Room;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +13,16 @@ import org.springframework.transaction.annotation.Transactional;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomQueryDslRepository {
 
     Optional<ChatRoom> findByRoom(Room room);
+
+    @Query("""
+                select distinct c
+                from ChatRoom c
+                join c.room r
+                join r.participants rp
+                where rp.member.id = :memberId
+                order by c.lastMessageTime desc nulls last
+            """)
+    Slice<ChatRoom> findParticipatingChatRooms(Pageable pageable, Long memberId);
 
     @Transactional
     @Modifying
