b4dfe89fc9592cea07ff5312f90d375fc56714ae
jae woo Jung <awfjol2008@gmail.com>
2026-01-23 19:52:23 +0900

    fix: 신규 쿼리 삭제 및 기존의 참여중인 채팅 조회 쿼리 복구

--- src/main/java/com/gobongbob/festamate/domain/chat/persistence/ChatRoomRepository.java
+++ src/main/java/com/gobongbob/festamate/domain/chat/persistence/ChatRoomRepository.java
@@ -3,8 +3,6 @@ package com.gobongbob.festamate.domain.chat.persistence;
 import com.gobongbob.festamate.domain.chat.domain.ChatRoom;
 import com.gobongbob.festamate.domain.room.domain.Room;
 import java.util.Optional;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,16 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomQueryDslRepository {
 
     Optional<ChatRoom> findByRoom(Room room);
-
-    @Query("""
-                select distinct c
-                from ChatRoom c
-                join c.room r
-                join r.participants rp
-                where rp.member.id = :memberId
-                order by c.lastMessageTime desc nulls last, c.createdAt desc
-            """)
-    Slice<ChatRoom> findParticipatingChatRooms(Pageable pageable, Long memberId);
 
     @Transactional
     @Modifying
